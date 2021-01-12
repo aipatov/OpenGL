9f6490fd67eda66496380a88b5b4b379016cf1d4
aipatov <56915438+aipatov@users.noreply.github.com>
2021-01-13 02:42:10 +0300

    Shader class

---
+++ src/Renderer/ShaderProgram.cpp
@@ -0,0 +1,90 @@
+#include "ShaderProgram.h"
+#include <iostream>
+
+namespace rd {
+	ShaderProgram::ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
+	{
+		GLuint vertexShaderID;
+		if (!createShader(vertexShader, GL_VERTEX_SHADER, vertexShaderID))
+		{
+			std::cerr << "VERTEX SHADER: Compile-time-error" << std::endl;
+			return;
+		}
+		GLuint fragmentShaderID;
+		if (!createShader(fragmentShader, GL_FRAGMENT_SHADER, fragmentShaderID))
+		{
+			std::cerr << "FRAGMENT SHADER: Compile-time-error" << std::endl;
+			glDeleteShader(vertexShaderID);
+			return;
+		}
+
+		m_ID = glCreateProgram();
+		glAttachShader(m_ID, vertexShaderID);
+		glAttachShader(m_ID, fragmentShaderID);
+		glLinkProgram(m_ID);
+
+		GLint success;
+		glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			GLchar infolog[1024];
+			glGetShaderInfoLog(m_ID, 1024, nullptr, infolog);
+			std::cerr << "ERROR::SHADER: Link-time-error:\n" << infolog << std::endl;
+		}
+		else
+			m_bCompiled = true;
+
+		glDeleteShader(vertexShaderID);
+		glDeleteShader(fragmentShaderID);
+	}
+
+	bool ShaderProgram::createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID)
+	{
+		shaderID = glCreateShader(shaderType);
+		const char* code = source.c_str();
+		glShaderSource(shaderID, 1, &code, nullptr);
+		glCompileShader(shaderID);
+
+		GLint success;
+		glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			GLchar infolog[1024];
+			glGetShaderInfoLog(shaderID, 1024, nullptr, infolog);
+			std::cerr << "ERROR::SHADER: Compile-time-error:\n" << infolog << std::endl;
+			return false;
+		}
+		return true;
+	}
+
+	ShaderProgram::~ShaderProgram()
+	{
+		glDeleteProgram(m_ID);
+	}
+
+	void ShaderProgram::use() const
+	{
+		glUseProgram(m_ID);
+	}
+
+	ShaderProgram& ShaderProgram::operator=(ShaderProgram&& shaderProgram) noexcept
+	{
+		glDeleteProgram(m_ID);
+		m_ID = shaderProgram.m_ID;
+		m_bCompiled = shaderProgram.m_bCompiled;
+
+		shaderProgram.m_ID = 0;
+		shaderProgram.m_bCompiled = false;
+
+		return *this;
+	}
+
+	ShaderProgram::ShaderProgram(ShaderProgram&& shaderProgram) noexcept
+	{
+		m_ID = shaderProgram.m_ID;
+		m_bCompiled = shaderProgram.m_bCompiled;
+
+		shaderProgram.m_ID = 0;
+		shaderProgram.m_bCompiled = false;
+	}
+}
